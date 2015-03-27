65f4ef3e90e72b4d12b5b4d39cd0c76c1aa6ae20
lianwei <lianwei@microsoft.com>
2015-03-27 11:37:39 +0800

    Refomat js file structure

---
+++ FrontEnd/app/js/directives.js
@@ -0,0 +1,20 @@
+angular.module('directives', [])
+/**
+ * backToTop Directive
+ * @param  {Function} $anchorScroll
+ *
+ * @description Ensure that the browser scrolls when the anchor is clicked
+ */
+.directive('backToTop', ['$anchorScroll', '$location', function($anchorScroll, $location) {
+  return function link(scope, element) {
+    element.on('click', function(event) {
+      $location.hash('');
+      scope.$apply($anchorScroll);
+    });
+  };
+}])
+.directive('scrollYOffsetElement', ['$anchorScroll', function($anchorScroll) {
+  return function(scope, element) {
+    $anchorScroll.yOffset = element;
+  };
+}]);
