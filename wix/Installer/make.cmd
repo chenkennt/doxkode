385098a23e9186354c122f0d9beb5d4f18a7a63e
Liangying Wei <lianwei@microsoft.com>
2015-04-12 23:42:38 +0800

    Minor fix...however website's preview and publish is to current folder and thus not working, consider change api generated result to current folder?

--- wix/Installer/make.cmd
+++ wix/Installer/make.cmd
@@ -1,4 +1,4 @@
-wix\heat.exe dir "..\..\Drop\debug\BuildMeta" -gg -sfrag -templatefragment -dr BUILDMETADIR -cg BuildMetaFiles -var var.BuildMetaDropFolder -out obj\buildmetafrag.wxs -sw5150 -sw5151
+wix\heat.exe dir "..\..\Drop\debug\BuildMeta" -gg -sfrag -templatefragment -dr INSTALLDIR -cg BuildMetaFiles -var var.BuildMetaDropFolder -out obj\buildmetafrag.wxs -sw5150 -sw5151
 wix\heat.exe dir "..\..\Drop\debug\VsixSource" -gg -sfrag -templatefragment -dr DocascodeVsixDir -cg DocascodeVsixFiles -var var.VsixDropFolder -out obj\vsixfrag.wxs -sw5150 -sw5151
 
 wix\candle.exe  -ext WixVsExtension installer.wxs obj\buildmetafrag.wxs obj\vsixfrag.wxs -dBuildMetaDropFolder="..\..\Drop\debug\BuildMeta" -dVsixDropFolder="..\..\Drop\debug\VsixSource" 
