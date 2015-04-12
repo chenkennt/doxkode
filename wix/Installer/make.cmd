0baae82b9fa2f2fe3b0dbab6731a31ebc0d45bc3
Liangying Wei <lianwei@microsoft.com>
2015-04-12 22:12:54 +0800

    Install to all users work however need a refresh

--- wix/Installer/make.cmd
+++ wix/Installer/make.cmd
@@ -1,3 +1,5 @@
-wix\heat.exe dir "..\..\Drop\debug\BuildMeta" -gg -sfrag -templatefragment -dr BUILDMETADIR -cg BuildMetaFiles -var var.BuildMetaDropFolder -out buildmetafrag.wxs -sw5150 -sw5151
-wix\candle.exe  -ext WixVsExtension installer.wxs buildmetafrag.wxs -dBuildMetaDropFolder="..\..\Drop\debug\BuildMeta"
-wix\light.exe -ext WixVsExtension -out docascode.msi buildmetafrag.wixobj installer.wixobj
+wix\heat.exe dir "..\..\Drop\debug\BuildMeta" -gg -sfrag -templatefragment -dr BUILDMETADIR -cg BuildMetaFiles -var var.BuildMetaDropFolder -out obj\buildmetafrag.wxs -sw5150 -sw5151
+wix\heat.exe dir "..\..\Drop\debug\VsixSource" -gg -sfrag -templatefragment -dr DocascodeVsixDir -cg DocascodeVsixFiles -var var.VsixDropFolder -out obj\vsixfrag.wxs -sw5150 -sw5151
+
+wix\candle.exe  -ext WixVsExtension installer.wxs obj\buildmetafrag.wxs obj\vsixfrag.wxs -dBuildMetaDropFolder="..\..\Drop\debug\BuildMeta" -dVsixDropFolder="..\..\Drop\debug\VsixSource" 
+wix\light.exe -ext WixVsExtension -out docascode.msi vsixfrag.wixobj buildmetafrag.wixobj installer.wixobj
