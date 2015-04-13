d568eed313b8b596fea7364d95a458eea9bf523f
lianwei <lianwei@microsoft.com>
2015-04-13 16:20:47 +0800

    Refactor folder structure

--- Installer/make.cmd
+++ Installer/make.cmd
@@ -15,5 +15,5 @@ SET OUTPUTFOLDER=%1
 wix\heat.exe dir "..\..\Drop\debug\BuildMeta" -gg -sfrag -templatefragment -dr INSTALLDIR -cg BuildMetaFiles -var var.BuildMetaDropFolder -out obj\buildmetafrag.wxs -sw5150 -sw5151
 wix\heat.exe dir "..\..\Drop\debug\VsixSource" -gg -sfrag -templatefragment -dr DocascodeVsixDir -cg DocascodeVsixFiles -var var.VsixDropFolder -out obj\vsixfrag.wxs -sw5150 -sw5151
 
-wix\candle.exe  -ext WixVsExtension installer.wxs obj\buildmetafrag.wxs obj\vsixfrag.wxs -dBuildMetaDropFolder="..\..\Drop\debug\BuildMeta" -dVsixDropFolder="..\..\Drop\debug\VsixSource" 
+wix\candle.exe  -ext WixVsExtension installer.wxs obj\buildmetafrag.wxs obj\vsixfrag.wxs -dBuildMetaDropFolder="..\..\..\Drop\debug\BuildMeta" -dVsixDropFolder="..\..\..\Drop\debug\VsixSource" 
 wix\light.exe -ext WixVsExtension -out "%OUTPUTFOLDER%\docascode.msi" vsixfrag.wixobj buildmetafrag.wixobj installer.wixobj
