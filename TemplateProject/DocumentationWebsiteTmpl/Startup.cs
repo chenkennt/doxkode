3fabdee84bb13f72af7479b8d5240880dbd839c0
lianwei <lianwei@microsoft.com>
2015-04-13 15:08:30 +0800

    Add kproj template

---
+++ TemplateProject/DocumentationWebsiteTmpl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace DocumentationWebsiteTmpl
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+        }
+    }
+}
