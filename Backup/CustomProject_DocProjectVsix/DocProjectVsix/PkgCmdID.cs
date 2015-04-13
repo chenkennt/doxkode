c03d325198adacbcd93f2e018129315765477b15
lianwei <lianwei@microsoft.com>
2015-04-13 11:51:58 +0800

    Add current CustomProjectVsix as backup

---
+++ Backup/CustomProject_DocProjectVsix/DocProjectVsix/PkgCmdID.cs
@@ -0,0 +1,11 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace MicrosoftIT.DocProject
+{
+    static class PkgCmdIDList
+    {
+        public const uint PublishToGithubCommand = 0x211;
+    };
+}
